7af1acc371a90009bb9d130fc8b9d2e1b91ddff9
Sourav Mazumdar <sourav.mozu@gmail.com>
2024-08-01 21:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+but=document.getElementsByTagName('input')[0]
+// console.log(but)
+but.addEventListener('click',func)
+sel=document.getElementById('colorSelect')
+options=Array.from(sel.options)
+optionsText=options.map((value=> value.textContent))
+
+function func(){
+	deletEle=sel.value
+	optionsText=optionsText.filter(value=> value!=deletEle)
+	sel.innerHTML=''
+	
+	optionsText.forEach(value=> {
+		newOp= document.createElement('option')
+		newOp.innerText=value
+		sel.appendChild(newOp)
+	} )
+}
